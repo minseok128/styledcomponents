169c1a9f46c4b1f70e7daee570cf3b7d6c15fc3b
Min-Seok Chang <minseok128128@gmail.com>
2020-05-06 01:23:29 +0900

    #2 HAHA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import styled, { createGlobalStyle, css } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding:0;
+    margin:0;
+  }
+`;
+
+const awesomeCard = css`
+  font-family: "Times New Roman", Times, serif;
+`;
+
+const Input = styled.input.attrs({
+  required: true,
+})`
+  border-radius: 5px;
+  margin: 10px;
+  ${awesomeCard};
+`;
+
+const Contianer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #f39c12;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Contianer>
+          <Input placeholder="good night" />
+        </Contianer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
